8ecb4a7af75315ad01b52b034d9c03190a16d8e5
Lokesh-ig <lokesh2005lokesh15@gmail.com>
2026-08-27 16:12:48 +0530

    Production hardening: Added Procfile, WhiteNoise, STATIC_ROOT, and JWT dependencies for Django Gateway

--- backend/gateway_django/core/settings.py
+++ backend/gateway_django/core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
